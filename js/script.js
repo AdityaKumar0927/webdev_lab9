d73e84214aed36e9df55df9c05b0f5dddc99411f
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2024-11-04 22:58:04 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+function init() {
+  const entryInput = document.getElementById('entryinput');
+  const entryButton = document.getElementById('entrybutton');
+  const textOutput = document.getElementById('textoutput');
 
+  entryButton.addEventListener('click', function() {
+    const inputText = entryInput.value;
+    const name = "Aditya Kumar"; 
+    const alertMessage = `${name}: ${inputText}`;
+    
+    alert(alertMessage);
+    textOutput.textContent = inputText;
+  });
+}
+
+window.addEventListener('load', init);
